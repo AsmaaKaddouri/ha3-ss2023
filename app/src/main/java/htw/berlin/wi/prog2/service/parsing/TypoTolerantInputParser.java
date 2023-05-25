6be6882e5dbd865438072d44c3bcfecb7c6f88c0
Asmaa Kaddouri <Asmaa.Kaddouri@Student.HTW-Berlin.de>
2023-05-25 08:58:58 +0200

    die Methode idsAndCountFromInput

--- app/src/main/java/htw/berlin/wi/prog2/service/parsing/TypoTolerantInputParser.java
+++ app/src/main/java/htw/berlin/wi/prog2/service/parsing/TypoTolerantInputParser.java
@@ -1,11 +1,23 @@
 package htw.berlin.wi.prog2.service.parsing;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class TypoTolerantInputParser implements ExtendableInputParser {
 
     @Override
     public Map<Long, Integer> idsAndCountFromInput(String inputLine, Map<String, Long> keywordsToIds) {
-        return Map.of(); // TODO hier implementieren und die korrekte Map zurückgeben
+        Map<Long, Integer> idsAndCountInput= new HashMap<>();
+
+        String[] words = inputLine.split("[ ,.-]");
+
+        for (String word : words) {
+            Long id = keywordsToIds.get(word);
+            if(id != null) {
+                idsAndCountInput.put(id, idsAndCountInput.getOrDefault(id, 0) + 1);
+
+            }
+        }
+        return idsAndCountInput; // TODO hier implementieren und die korrekte Map zurückgeben
     }
 }
